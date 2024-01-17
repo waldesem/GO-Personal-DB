00286ccf7f1bd75d6cf3f8898123006cb247ead3
Vladimir Semenenko <wsemenenko@gmail.com>
2024-01-17 16:02:17 +0300

    Update admin controller

--- backend/app/controllers/admin.go
+++ backend/app/controllers/admin.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"encoding/json"
-	"log"
 	"os"
 	"strconv"
 
@@ -13,12 +12,18 @@ import (
 	"backend/platform/database"
 )
 
+type Userdata struct {
+	UserName string `json:"username"`
+	FullName string `json:"full_name"`
+	Email    string `json:"email"`
+}
+
 func GetUsers(c *fiber.Ctx) error {
 	db := database.OpenDb()
 	var users []models.User
 	db.Find(&users)
 
-	result, err := json.Marshal(users)
+	result, err := json.Marshal(&users)
 
 	if err != nil {
 		return c.Status(500).JSON(err)
@@ -37,17 +42,19 @@ func GetUser(c *fiber.Ctx) error {
 		if err != nil {
 			return c.Status(400).JSON("Invalid ID")
 		}
-		if user.ID != uint(id) {
+		if user.ID == uint(id) {
 			user.Blocked = !user.Blocked
 		}
 	case "drop":
-		user.Password = utils.GeneratePassword(os.Getenv("DEFAULT_PASSWORD"))
+		pswd, ok := os.LookupEnv("DEFAULT_PASSWORD")
+		if !ok {
+			return c.Status(500).JSON(ok)
+		}
+		user.Password = utils.GeneratePassword(pswd)
 	}
 	db.Save(&user)
 
-	db.First(&user, c.Params("id"))
 	result, err := json.Marshal(user)
-
 	if err != nil {
 		return c.Status(500).JSON(err)
 	}
@@ -55,59 +62,57 @@ func GetUser(c *fiber.Ctx) error {
 }
 
 func PostUser(c *fiber.Ctx) error {
-	userdata := struct {
-		UserName string `json:"username"`
-		FullName string `json:"full_name"`
-		Email    string `json:"email"`
-	}{}
+	var userdata Userdata
+	var user models.User
 
 	if err := c.BodyParser(&userdata); err != nil {
 		return c.Status(500).JSON(err)
 	}
+
 	db := database.OpenDb()
-	var user models.User
 	db.Where("user_name LIKE ?", "%"+userdata.UserName+"%").First(&user)
 
-	if user.UserName != userdata.UserName {
+	if user.ID == 0 {
 		user.FullName = userdata.FullName
 		user.UserName = userdata.UserName
 		user.Email = userdata.Email
-		user.Password = utils.GeneratePassword(os.Getenv("DEFAULT_PASSWORD"))
+		pswd, ok := os.LookupEnv("DEFAULT_PASSWORD")
+		if !ok {
+			return c.Status(500).JSON(ok)
+		}
+		user.Password = utils.GeneratePassword(pswd)
+
 	} else {
 		return c.Status(400).JSON("User already exists")
 	}
 	db.Create(&user)
-	return c.Status(201).JSON(user)
+	return c.Status(201).JSON("User created")
 }
 
 func PatchUser(c *fiber.Ctx) error {
-	userdata := struct {
-		UserName string `json:"username"`
-		FullName string `json:"full_name"`
-		Email    string `json:"email"`
-	}{}
+	var userdata Userdata
+	var user models.User
 
 	if err := c.BodyParser(&userdata); err != nil {
 		return c.Status(500).JSON(err)
 	}
+
 	db := database.OpenDb()
-	var user models.User
 	db.Where("user_name LIKE ?", "%"+userdata.UserName+"%").First(&user)
 
 	if user.UserName == userdata.UserName {
 		user.FullName = userdata.FullName
 		user.UserName = userdata.UserName
 		user.Email = userdata.Email
-		user.Password = utils.GeneratePassword(os.Getenv("DEFAULT_PASSWORD"))
 	}
 	db.Create(&user)
 	return c.Status(201).JSON(user)
 }
 
 func DeleteUser(c *fiber.Ctx) error {
-	db := database.OpenDb()
 	var user models.User
 
+	db := database.OpenDb()
 	db.First(&user, c.Params("id"))
 	db.Delete(&user)
 
@@ -115,148 +120,141 @@ func DeleteUser(c *fiber.Ctx) error {
 }
 
 func GetRoles(c *fiber.Ctx) error {
-	db := database.OpenDb()
 	var user models.User
+	var roleModel models.Role
+	var hasRole bool
+
+	db := database.OpenDb()
 	db.
 		Preload("Roles").
 		First(&user, c.Params("user_id"))
 
 	for _, role := range user.Roles {
-		if role.NameRole != c.Params("value") {
-			var role models.Role
-			db.Where("name_role LIKE ?", "%"+c.Params("value")+"%").First(&role)
-			user.Roles = append(user.Roles, role)
-			db.Save(&user)
+		if role.NameRole == c.Params("value") {
+			hasRole = true
 			break
 		}
 	}
-	db.First(&user, c.Params("user_id"))
-	return c.Status(200).JSON(user)
+	if !hasRole {
+		db.
+			Where("name_role LIKE ?", "%"+c.Params("value")+"%").
+			First(&roleModel)
+		user.Roles = append(user.Roles, roleModel)
+		db.Save(&user)
+	}
+	return c.Status(200).JSON("Role added")
 }
 
 func GetGroups(c *fiber.Ctx) error {
-	db := database.OpenDb()
 	var user models.User
+	var groupModel models.Group
+	var hasGroup bool
+
+	db := database.OpenDb()
 	db.
 		Preload("Groups").
 		First(&user, c.Params("user_id"))
 
 	for _, group := range user.Groups {
-		if group.NameGroup != c.Params("value") {
-			var group models.Group
-			db.Where("name_group LIKE ?", "%"+c.Params("value")+"%").First(&group)
-			user.Groups = append(user.Groups, group)
-			db.Save(&user)
+		if group.NameGroup == c.Params("value") {
+			hasGroup = true
 			break
 		}
-
 	}
-	db.First(&user, c.Params("user_id"))
-	return c.Status(200).JSON(user)
+	if !hasGroup {
+		db.Where("name_group LIKE ?", "%"+c.Params("value")+"%").First(&groupModel)
+		user.Groups = append(user.Groups, groupModel)
+		db.Save(&user)
+	}
+	return c.Status(200).JSON("Group added")
 }
 
 func DelRoles(c *fiber.Ctx) error {
-	db := database.OpenDb()
 	var user models.User
+	var roleModel models.Role
 
+	db := database.OpenDb()
 	db.
 		Preload("Roles").
 		First(&user, c.Params("user_id"))
 
 	for i, role := range user.Roles {
 		if role.NameRole == c.Params("value") {
-			var role models.Role
-			db.Where("name_role LIKE ?", "%"+c.Params("value")+"%").First(&role)
+			db.Where("name_role LIKE ?", "%"+c.Params("value")+"%").First(&roleModel)
 			user.Roles = append(user.Roles[:i], user.Roles[i+1:]...)
 			db.Save(&user)
 			break
 		}
 	}
-	db.First(&user, c.Params("user_id"))
-	return c.Status(200).JSON(user)
+	return c.Status(200).JSON("Role deleted")
 }
 
 func DelGroups(c *fiber.Ctx) error {
-	db := database.OpenDb()
 	var user models.User
+	var groupModel models.Group
 
+	db := database.OpenDb()
 	db.
 		Preload("Groups").
 		First(&user, c.Params("user_id"))
 
 	for i, group := range user.Groups {
 		if group.NameGroup == c.Params("value") {
-			var group models.Group
-			db.Where("name_group LIKE ?", "%"+c.Params("value")+"%").First(&group)
+			db.Where("name_group LIKE ?", "%"+c.Params("value")+"%").First(&groupModel)
 			user.Groups = append(user.Groups[:i], user.Groups[i+1:]...)
 			db.Save(&user)
 			break
 		}
 	}
-	db.First(&user, c.Params("user_id"))
-	return c.Status(200).JSON(user)
+	return c.Status(200).JSON("Group deleted")
 }
 
 func PostTablesRows(c *fiber.Ctx) error {
-	db := database.OpenDb()
+	var pagination = 16
+	var hasPrev, hasNext bool
+
 	intPage, err := strconv.Atoi(c.Params("page"))
 	if err != nil {
 		intPage = 1
 	}
-	var pagination = 16
-	var hasPrev, hasNext bool
-	items := []string{"user", "role", "group", "report", "resume", "connect"}
 
 	searchData := struct {
 		Search string `json:"search"`
 	}{}
-	if err := c.BodyParser(&searchData); err != nil {
-		log.Println(err)
+	err = c.BodyParser(&searchData)
+	if err != nil {
+		searchData.Search = ""
 	}
+
 	result := []interface{}{}
 
-	for i := range items {
-		if items[i] == c.Params("item") {
-			query := db.
-				Table(items[i]).
-				Limit(pagination).
-				Offset(pagination * (intPage - 1)).
-				Order("id desc")
+	db := database.OpenDb()
+	query := db.
+		Table(c.Params("item")).
+		Limit(pagination).
+		Offset(pagination * (intPage - 1)).
+		Order("id desc")
 
-			if searchData.Search != "" {
-				query = query.Where("id = ?", searchData.Search)
-			}
+	if searchData.Search != "" {
+		query = query.Where("id = ?", searchData.Search)
+	}
+	query.Find(&result)
 
-			query.Find(&result)
-		} else {
-			query := db.
-				Table(c.Params("item")).
-				Limit(pagination).
-				Offset(pagination * (intPage - 1)).
-				Order("id desc")
-
-			if searchData.Search != "" {
-				query = query.Where("person_id = ?", searchData.Search)
-			}
-
-			query.Find(&result)
-		}
-
-		if intPage > 1 {
-			hasPrev = true
-		}
-		if len(items[i]) == pagination {
-			hasNext = true
-		}
+	if intPage > 1 {
+		hasPrev = true
+	}
+	if len(result) == pagination {
+		hasNext = true
 	}
 	return c.JSON(fiber.Map{"result": result, "hasNext": hasNext, "hasPrev": hasPrev})
 }
 
 func DelTableRows(c *fiber.Ctx) error {
-	database.OpenDb().
-		Table(c.Params("item")).
-		Where("id = ?", c.Params("id")).
-		Delete(c.Params("item"))
+	row := struct{}{}
+	db := database.OpenDb()
+	db.Table(c.Params("item"), c.Params("id")).First(row)
+	db.Delete(row)
+	db.Commit()
 
-	return c.Status(204).JSON("Deleted")
+	return c.Status(204).JSON("Row deleted")
 }
